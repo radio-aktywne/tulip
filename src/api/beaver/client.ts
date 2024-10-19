61c2e783fe25a807ecf2580d7ed9ed136fe00e5a
Sebastian Pietras <me@spietras.dev>
2024-10-19 22:49:13 +0200

    Changed ports (#9)

--- src/api/beaver/client.ts
+++ src/api/beaver/client.ts
@@ -7,7 +7,7 @@ const scheme = process.env.TULIP__BEAVER__SCHEME || "http";
 const host = process.env.TULIP__BEAVER__HOST || "localhost";
 const port =
   process.env.TULIP__BEAVER__PORT === undefined
-    ? 35000
+    ? 10500
     : process.env.TULIP__BEAVER__PORT;
 const path = (process.env.TULIP__BEAVER__PATH || "")
   // Ensure path starts with a slash
